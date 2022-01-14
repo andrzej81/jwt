2b5c9b8949d3d0aa745e877905410d61a7f84fcb
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-14 19:52:36 +0000

    create sample API

---
+++ README.md
@@ -0,0 +1,13 @@
+#Sample API created in Node.js and Express
+
+##use e.g Postman, curl
+##npm install (install dependencies)
+
+##node app.js to run API server
+
+- GET "/" to received message "Hello API"
+- POST "create new JWT token from json data {
+  "username" : "put your username",
+  "password" : "put your password"
+  }
+- GET /list_movies to received json data from server
--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-const http = require("http");
-const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+let express = require("express");
+let app = express();
+let dotenv = require("dotenv");
+let bodyParser = require("body-parser");
 
-const hostname = "127.0.0.1";
+dotenv.config();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post("/getToken", urlencodedParser, (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  let payload = { username: password };
+
+  let accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    algorithm: "HS256",
+    expiresIn: process.env.REFRESH_TOKEN_LIFE,
+  });
+
+  res.send(JSON.stringify(accessToken));
+});
+
+app.get("/list_movies", (req, res) => {
+  fs.readFile(__dirname + "/" + "movies.json", "utf8", (err, data) => {
+    res.end(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.end("Hello API");
 });
