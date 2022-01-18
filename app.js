fa4d3edb5e504b8a5949b3837c4a424fc2d1f520
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-18 18:31:09 +0000

    add another route

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.get("/", (req, res) => {
   res.send("Welcome to out site");
 });
 
+app.get("/login", (req, res) => {
+  console.log("route /login");
+  console.log(req.botInfo);
+
+  res.send("On login page!");
+});
+
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
 );
