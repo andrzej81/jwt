99d7d845338b7f51eab13fea4cf7d0f34feabee4
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-18 16:18:32 +0000

    check user client

--- middleware/checkBot.js
+++ middleware/checkBot.js
@@ -1,4 +1,9 @@
 module.exports = function checkBot(req, res, next) {
   console.log("checkBot middleware working.");
+
+  console.log(req.headers["user-agent"]);
+  console.log(req.headers["host"]);
+  console.log(req.socket.remoteAddress);
+  console.log(req.ip);
   next();
 };
