be342c8e0fe7f404963fbf6aaf0b6810984ba2ac
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-18 17:14:28 +0000

    add check bot patterns map and simple logic

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const port = 3000;
 
 app.use(checkBot);
 
-app.get("/", (req, res) => res.send("Hello to my lovely website!"));
+app.get("/", (req, res) => {
+  console.log(" route /");
+  console.log(req.botInfo);
+
+  res.send("Welcome to out site");
+});
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
--- middleware/checkBot.js
+++ middleware/checkBot.js
@@ -1,4 +1,29 @@
+const listBotsPattern = new Map([
+  ["Headless Chrome", /HeadlessChrome/],
+  ["Wget", /[wW]get/],
+  ["Python urllib", /Python\-urllib/],
+  ["PHP crawl", /phpcrawl/],
+  ["PhantomJS", /PhantomJS/],
+]);
+
+// analyse incoming reques by ise user-agent header field
+function isKnownBotUserAgent(userAgent) {
+  for (const [botName, botPattern] of listBotsPattern.entries()) {
+    if (userAgent.match(botPattern)) {
+      return {
+        isBot: true,
+        nameBot: botName,
+      };
+    }
+  }
+
+  return {
+    isBot: false,
+  };
+}
+
 module.exports = function checkBot(req, res, next) {
+  req.botInfo = isKnownBotUserAgent(req.header("User-Agent"));
   console.log("checkBot middleware working.");
 
   console.log(req.headers["user-agent"]);
